dd37955cb132bb006a53eb7d779dd92efc5e974b
Bryan Owens <djbryan3540@gmail.com>
2014-04-19 22:11:54 -0500

    Loading files from template
    BlakKat CM11 theme Template upload set1

---
+++ gen/org/cyanogenmod/djdarkknight96/blakkatlite/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.cyanogenmod.djdarkknight96.blakkatlite;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
